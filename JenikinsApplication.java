36838a7aa035f5c91e83face9ac3e4ac7f60aa21
[vidya] <srividyamadgula@gmail.com>
2022-05-18 20:56:26 +0530

    PROGRAMMING AREA

--- JenikinsApplication.java
+++ JenikinsApplication.java
@@ -3,6 +3,8 @@ public class JenikinsApplication {
 	public static void main(String[] args) {
 		
               System.out.println("Hello world");
+			  System.out.println(" WELCOME TO JENKINS PROJECT ");
+			  
 	}
 
 }
